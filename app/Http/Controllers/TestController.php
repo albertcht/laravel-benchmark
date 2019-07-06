62539a472ee7d04fdc72e42f96790f421317e83a
Albert Chen <albert.cht@gmail.com>
2019-07-06 23:22:17 +0800

    update routes

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class TestController extends BaseController
+{
+    public function test()
+    {
+        return ['foo' => 'bar'];
+    }
+}
