d39d1aa4e44b7f089640ba643f53c77a1467e925
Albert Chen <albert.cht@gmail.com>
2019-07-06 17:59:53 +0800

    first commit

---
+++ routes/api.php
@@ -0,0 +1,621 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+
+Route::get('/test2', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+
+Route::get('/test3', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test4', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test5', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test6', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test7', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test8', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test9', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test10', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test11', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test12', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test13', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test14', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test15', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test16', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test17', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test18', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test19', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test20', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test21', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test22', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test23', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test24', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test25', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test26', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test27', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test28', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test29', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test30', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test31', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test32', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test33', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test34', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test35', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test36', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test37', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test38', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test39', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test40', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test41', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test42', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test43', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test44', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test45', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test46', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test47', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test48', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test49', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test50', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test51', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test52', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test53', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test54', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test55', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test56', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test57', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test58', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test59', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test60', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test61', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test62', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test63', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test64', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test65', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test66', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test67', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test68', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test69', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test70', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test71', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test72', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test73', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test74', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test75', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test76', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test77', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test78', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test79', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test80', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test81', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test82', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test83', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test84', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test85', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test86', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test87', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test88', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test89', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test90', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test91', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test92', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test93', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test94', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test95', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test96', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test97', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test98', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test99', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test100', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test101', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test102', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test103', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test104', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test105', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test106', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test107', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test108', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test109', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test100', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test111', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test112', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test113', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test114', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test115', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test116', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test117', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test118', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test119', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test120', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test121', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test122', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test123', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test124', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test125', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test126', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test127', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test128', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test129', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test130', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test131', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test132', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test133', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test134', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test135', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test136', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test137', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test138', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test139', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test140', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test141', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test142', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test143', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test144', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test145', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test146', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test147', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test148', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test149', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test150', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test151', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test152', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test153', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test154', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test155', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test156', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test157', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test158', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test159', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test160', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test161', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test162', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test163', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test164', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test165', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test166', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test167', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test168', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test169', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test170', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test171', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test172', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test173', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test174', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test175', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test176', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test177', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test178', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test179', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test180', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test181', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test182', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test183', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test184', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test185', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test186', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test187', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test188', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test189', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test190', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test191', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test192', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test193', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test194', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test195', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test196', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test197', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test198', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test199', function (Request $request) {
+    return ['foo' => 'bar'];
+});
+Route::get('/test200', function (Request $request) {
+    return ['foo' => 'bar'];
+});
